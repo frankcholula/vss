c0b36649d6c4b5165a67a00d2691937f704abe6d
frankcholula <lu.phrank@gmail.com>
2024-10-19 12:11:54 +0100

    start to debug.

--- assignment1/visual_search.py
+++ assignment1/visual_search.py
@@ -147,6 +147,9 @@ def main():
         session_manager.update_recompute(False)
 
     img2descriptors = descriptor.get_image_descriptor_mapping()
+    # TODO: debug
+    for img, desc in img2descriptors.items():
+        print(f"Image: {img}, Descriptor: {desc.shape}")
 
     # Button to select a random image
     cols[2].markdown("<div style='width: 1px; height: 28px'></div>", unsafe_allow_html=True)
