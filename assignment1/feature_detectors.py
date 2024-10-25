e09ba0e7de384031df874decdd01f9e0dd73326d
frankcholula <lu.phrank@gmail.com>
2024-10-25 01:23:53 +0100

    implemented sift descriptor.

--- assignment1/feature_detectors.py
+++ assignment1/feature_detectors.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 class FeatureDetector:
     def __init__(self, detector_type: str):
         self.detector_type = detector_type
@@ -15,3 +16,7 @@ class FeatureDetector:
                 raise ValueError(f"Invalid detector type: {self.detector_type}")
         return detector
     
+    def detect_keypoints_compute_descriptors(self, img: np.ndarray):
+        gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+        keypoints, descriptors = self.detector.detectAndCompute(gray, None)
+        return keypoints, descriptors
--- assignment1/tests/test_feature_detectors.py
+++ assignment1/tests/test_feature_detectors.py
@@ -1,17 +1,16 @@
+import cv2
 import unittest
+import numpy as np
 from assignment1.feature_detectors import FeatureDetector
-from assignment1.ground_truth import ImageLabeler
-
 class TestFeatureDetectors(unittest.TestCase):
     def setUp(self):
         DATASET_FOLDER = "MSRC_ObjCategImageDatabase_v2_local"
-        self.img_labeler = ImageLabeler(DATASET_FOLDER)
-        self.img1_filename = "1_1_s.bmp"
+        self.img1 = cv2.imread(f"{DATASET_FOLDER}/Images/1_1_s.bmp")
         self.fd = FeatureDetector("SIFT")
     
     def test_create_detector(self):
         self.assertIsNotNone(self.fd.detector, "The detector should not be None.")
     
-    def test_detect_keypoints(self):
-        # fd.detect_keypoints(self.img_labeler.load_img(self.img1_filename))
-        pass
+    def test_detect_keypoints_compute_descriptors(self):
+        keypoints, descriptors = self.fd.detect_keypoints_compute_descriptors(self.img1)
+        self.assertTrue(all(descriptor.shape[0] == 128 for descriptor in descriptors), "Each descriptor should have a shape of 128.")
